e7c5a34aa45ad5585dd5882e08dba331353281c8
Stephen Reish <spreish@ncsu.edu>
2022-05-23 18:18:17 -0400

    Got API to return a list of categories

--- react-trivia/blank.js
+++ react-trivia/blank.js
@@ -1,9 +1,38 @@
-<div>
-    <p>Results go here:</p>
-    {apiResults && apiResults.length ?
-        apiResults.map((apiResult, index) => (
-            <div key={index}>
-                <p>{index}</p>
-            </div>
+// ATTEMPTED syntax:
+
+return (
+    <div>
+        <p>Results go here:</p>
+        {apiResults && apiResults.length ?
+            apiResults.map((apiResult, index) => (
+            return (
+        <div key={index}>
+            <p>{index}</p>
+        </div>
         )) : null}
-</div>
+    </div>
+)
+        )
+    })}
+</div >
+)
+
+
+return (
+    <div>
+        <p>Results go here:</p>
+        {apiResults && apiResults.length ?
+            apiResults.map((apiResult, index) => {
+                return (
+                    <div key={index}>
+                        <p>{index}</p>
+                        {apiResult.content.map((eachCat) => (
+                            <div key={eachCat.index}>
+                                <h1>{eachCat.category}</h1>
+                            </div>
+                        ))}
+                    </div>
+                )
+            })}
+    </div>
+)
--- react-trivia/src/components/Categories.js
+++ react-trivia/src/components/Categories.js
@@ -2,32 +2,25 @@ import { useEffect, useState } from 'react';
 import axios from 'axios'
 
 export function Categories() {
-    const [apiResults, displayApiResults] = useState([])
+    const [catObjects, displayApiResults] = useState([])
 
 
     useEffect(() => {
         axios
             .get(`https://opentdb.com/api_category.php`)
             .then((res) => {
-                console.log(res.data)
-                displayApiResults(res.data)
+                console.log(res.data.trivia_categories)
+                displayApiResults(res.data.trivia_categories)
             })
     }, [])
 
     return (
         <div>
-            <p>Results go here:</p>
-            {apiResults.map((apiResult, index) => {
+            <p>The results are:</p>
+            {catObjects.map((catObject, index) => {
                 return (
                     <div key={index}>
-                        <p>{index}</p>
-                        <p>{apiResult.created_at}</p>
-                        <p>{apiResult.location}</p>
-                        {apiResult.content.map((eachCat) => (
-                            <div key={eachCat.index}>
-                                <h1>{eachCat.category}</h1>
-                            </div>
-                        ))}
+                        <p>{catObject.name}</p>
                     </div>
                 )
             })}
