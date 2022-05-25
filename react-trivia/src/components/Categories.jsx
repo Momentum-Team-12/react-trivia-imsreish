f4242793099529d498e0c9c16a3ff86aa6621483
Stephen Reish <spreish@ncsu.edu>
2022-05-25 15:56:32 -0400

    Popup displays link for future api call

--- react-trivia/src/components/Categories.jsx
+++ react-trivia/src/components/Categories.jsx
@@ -9,28 +9,34 @@ export function Categories() {
         axios
             .get(`https://opentdb.com/api_category.php`)
             .then((res) => {
-                console.log(res.data.trivia_categories)
+                // console.log(res.data.trivia_categories)
                 setCatObjects(res.data.trivia_categories)
             })
     },
         [])
 
-
-    const showCatName = (catName) => {
+    const showCatName = (catName, catId) => {
         console.log(catName)
         return (
-            window.alert(`you have selected ${catName}`)
+            window.alert(`https://opentdb.com/api.php?amount=10&category=${catId}`)
         )
     }
 
     return (
         <div>
+            {/* <div>
+                {catObjects.map((catObject, index) => (
+                    <p>Hello, {catObject.name} with {catObject.id}!</p>
+                ))}
+            </div> */}
+            <button onClick={() => showCatName()}>Click me</button>
             <p>The results are:</p>
             {catObjects.map((catObject, index) => {
                 const catName = catObject.name
+                const catId = catObject.id
                 return (
                     <div className="catContainer" key={index}>
-                        <button onClick={() => showCatName(catName)}>{catName}</button>
+                        <button onClick={() => showCatName(catName, catId)}>{catName}</button>
                     </div>
                 )
             })}
